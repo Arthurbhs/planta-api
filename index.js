f6424d0485c3694340f372faff7560836e8c12e7
Arthur_bhs <arthur.shekinarfoxy@gmail.com>
2024-06-12 22:00:01 -0300

    first test

---
+++ index.js
@@ -0,0 +1,31 @@
+
+const app = express();
+
+// Exemplo de dados de plantas
+const plants = [
+  { id: 1, name: 'Orquídea', origin: 'Tropical' },
+  { id: 2, name: 'Suculenta', origin: 'Deserto' },
+  { id: 3, name: 'Samambaia', origin: 'Floresta Tropical' },
+];
+
+// Rota para obter todas as plantas
+app.get('/plants', function (req, res) {
+  res.json(plants); // Retorna todas as plantas como um JSON
+});
+
+// Rota para obter uma planta específica pelo ID
+app.get('/plants/:id', function (req, res) {
+  const id = parseInt(req.params.id); // Obtém o parâmetro ID da URL e converte para número inteiro
+  const plant = plants.find(p => p.id === id); // Procura a planta pelo ID
+
+  if (!plant) {
+    res.status(404).send('Planta não encontrada'); // Se não encontrar a planta, retorna status 404
+  } else {
+    res.json(plant); // Retorna a planta encontrada como JSON
+  }
+});
+
+// Inicia o servidor na porta especificada ou na porta 3000
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Servidor está rodando na porta 3000');
+});
