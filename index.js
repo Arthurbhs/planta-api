ec4640c65de7cada85b607a4925f7615806d931d
Arthur_bhs <arthur.shekinarfoxy@gmail.com>
2024-06-12 22:11:23 -0300

    thrirt test

--- index.js
+++ index.js
@@ -1,35 +1,13 @@
-
 const express = require('express');
 const app = express();
 
-// Restante do seu código do Express aqui
-
-
-// Exemplo de dados de plantas
-const plants = [
-  { id: 1, name: 'Orquídea', origin: 'Tropical' },
-  { id: 2, name: 'Suculenta', origin: 'Deserto' },
-  { id: 3, name: 'Samambaia', origin: 'Floresta Tropical' },
-];
-
-// Rota para obter todas as plantas
-app.get('/plants', function (req, res) {
-  res.json(plants); // Retorna todas as plantas como um JSON
-});
-
-// Rota para obter uma planta específica pelo ID
-app.get('/plants/:id', function (req, res) {
-  const id = parseInt(req.params.id); // Obtém o parâmetro ID da URL e converte para número inteiro
-  const plant = plants.find(p => p.id === id); // Procura a planta pelo ID
-
-  if (!plant) {
-    res.status(404).send('Planta não encontrada'); // Se não encontrar a planta, retorna status 404
-  } else {
-    res.json(plant); // Retorna a planta encontrada como JSON
-  }
+// Rota para a rota raiz '/'
+app.get('/', (req, res) => {
+  res.send('Hello World');
 });
 
 // Inicia o servidor na porta especificada ou na porta 3000
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Servidor está rodando na porta 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor está rodando na porta ${PORT}`);
 });
